68bd1874a198def0e80dfbaf79bbf0c8a1bdf492
ajat <ajat.sudrajat76@gmail.com>
2018-02-26 17:42:46 +0700

    change lambda

--- src/main/java/com/test/restapi/config/APISecurityConfig.java
+++ src/main/java/com/test/restapi/config/APISecurityConfig.java
@@ -3,14 +3,11 @@ package com.test.restapi.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 
 @Configuration
 @EnableWebSecurity
@@ -26,18 +23,14 @@ public class APISecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
-        filter.setAuthenticationManager(new AuthenticationManager() {
-
-            @Override
-            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                String principal = (String) authentication.getPrincipal();
-                if (!principalRequestValue.equals(principal))
-                {
-                    throw new BadCredentialsException("The API key was not found or not the expected value.");
-                }
-                authentication.setAuthenticated(true);
-                return authentication;
+        filter.setAuthenticationManager(authentication -> {
+            String principal = (String) authentication.getPrincipal();
+            if (!principalRequestValue.equals(principal))
+            {
+                throw new BadCredentialsException("The API key was not found or not the expected value.");
             }
+            authentication.setAuthenticated(true);
+            return authentication;
         });
         httpSecurity.
             antMatcher("/api/**").
